df5d7bd7a07b3b4aecd60e8ab9478e0f3bf7c54f
Maddi Cummins <maddic9@uw.edu>
2020-02-17 22:10:09 -0800

    about page info added

--- capstone-site/src/components/about.js
+++ capstone-site/src/components/about.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import kyleimg from './img/Kyle.jpg';
+import thoaimg from './img/Thoa.jpg';
+import samimg from './img/Sam.jpg';
+import maddiimg from './img/Maddi.jpg';
 
 
 // This is the navigation bar component containing links that can reroute the page
@@ -16,11 +20,11 @@ class AboutPage extends Component {
                             <div className="card-body">
                                 <div className="row">
                                     <div className="col-sm-auto">
-                                        <img className="pb-3" src="img/card-club-icon.png" alt="the club suit symbol" />
+                                        <img src={kyleimg} alt="Picture of Kyle Avalani" width="172" height="172"/>
                                     </div>
                                     <div className="col-sm">
-                                        <h2 className="card-title">Clubs</h2>
-                                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, fuga?</p>
+                                        <h2 className="card-title">Kyle Avalani</h2>
+                                        <p className="card-text">Kyle has spent over two years working in the gaming industry developing VR games and has a passion for environmental activism. He loves gaming almost as much as he loves his cat, Copper.</p>
                                         <a className="btn btn-dark" href="#" role="button">Play</a>
                                     </div>
                                 </div>
@@ -34,11 +38,11 @@ class AboutPage extends Component {
                             <div className="card-body">
                                 <div className="row">
                                     <div className="col-sm-auto">
-                                        <img className="pb-3" src="img/card-diamond-icon.png" alt="the diamond suit symbol" />
+                                        <img src= {samimg} alt="Picture of Sam Buxton" width="172" height="172"/>
                                     </div>
                                     <div className="col-sm">
-                                        <h2 className="card-title">Diamonds</h2>
-                                        <p className="card-text">Quam, officiis ut adipisci pariatur veniam labore ipsum optio tenetur!</p>
+                                        <h2 className="card-title">Sam Buxton</h2>
+                                        <p className="card-text">As a former teacher and software developer, Sam hopes to use experiential learning to give young children an intuitive understanding of their environment and how it changes. He has a passion for education and the technical background to make the impossible possible.</p>
                                         <a className="btn btn-dark" href="#" role="button">Play</a>
                                     </div>
                                 </div>
@@ -52,11 +56,11 @@ class AboutPage extends Component {
                             <div className="card-body">
                                 <div className="row">
                                     <div className="col-sm-auto">
-                                        <img className="pb-3" src="img/card-heart-icon.png" alt="the heart suit symbol" />
+                                        <img src={maddiimg} alt="Picture of Maddi Cummins" width="172" height="172"/>
                                     </div>
                                     <div className="col-sm">
-                                        <h2 className="card-title">Hearts</h2>
-                                        <p className="card-text">Reiciendis doloremque voluptas veniam repellendus quisquam deleniti tenetur quia a.</p>
+                                        <h2 className="card-title">Maddi Cummins</h2>
+                                        <p className="card-text">Maddi has a passion for the environment and sustaining the ecosystem so it can continue to support the lives of all its inhabitants. Growing up gaming and with a strong background in coding, she is excited to apply what she knows to help kids help the world.</p>
                                         <a className="btn btn-dark" href="#" role="button">Play</a>
                                     </div>
                                 </div>
@@ -70,12 +74,13 @@ class AboutPage extends Component {
                             <div className="card-body">
                                 <div className="row">
                                     <div className="col-sm-auto">
-                                        <img className="pb-3" src="img/card-spade-icon.png" alt="the heart suit symbol" />
+                                        <img src={thoaimg} alt="Picture of Thoa Nguyen" width="172" height="172"/>
                                     </div>
                                     <div className="col-sm">
-                                        <h2 className="card-title">Spades</h2>
-                                        <p className="card-text">Maiores voluptate rem odit dolor temporibus asperiores maxime velit sapiente.</p>
-                                        <a className="btn btn-dark" href="#" role="button">Play</a>                  </div>
+                                        <h2 className="card-title">Thoa Nguyen</h2>
+                                        <p className="card-text">Thoa is passionate about leveraging technology for education and environmental sustainability. She is an active mentor for low-income students and advocates for improving classroom curriculums. </p>
+                                        <a className="btn btn-dark" href="#" role="button">Play</a>                  
+                                    </div>
                                 </div>
                             </div>
                         </div>
