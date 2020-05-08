5b47fa31bd0f7b2ee54723cc2cd3fcebe0ae79f0
Maddi Cummins <maddic9@uw.edu>
2020-05-07 17:03:03 -0700

    py code extended

--- unitOneScript.py
+++ unitOneScript.py
@@ -18,3 +18,37 @@ def block_placed_flower_pot():
     for index3 in range(5):
         mobs.spawn(TROPICAL_FISH, world(104, 69, 508))
 blocks.on_block_placed(FLOWER_POT, block_placed_flower_pot)
+def on_chat():
+    blocks.clone(world(108, 59, 568),
+        world(106, 60, 566),
+        world(129, 81, 561),
+        CloneMask.REPLACE,
+        CloneMode.FORCE)
+    blocks.clone(world(108, 59, 568),
+        world(106, 60, 566),
+        world(113, 81, 561),
+        CloneMask.REPLACE,
+        CloneMode.FORCE)
+    blocks.clone(world(108, 59, 568),
+        world(106, 60, 566),
+        world(121, 84, 569),
+        CloneMask.REPLACE,
+        CloneMode.FORCE)
+player.on_chat("cover", on_chat)
+def on_chat2():
+    mobs.spawn(POLAR_BEAR, world(120, 65, 565))
+player.on_chat("bearslave", on_chat2)
+blocks.clone(world(467, 63, 544),
+    world(467, 63, 544),
+    world(464, 64, 543),
+    CloneMask.REPLACE,
+    CloneMode.FORCE)
+gameplay.set_weather(CLEAR)
+mobs.kill(mobs.entities_by_type(POLAR_BEAR))
+while True:
+    loops.pause(500)
+    blocks.clone(world(131, 65, 561),
+        world(131, 65, 561),
+        world(114, 66, 560),
+        CloneMask.REPLACE,
+        CloneMode.FORCE)
