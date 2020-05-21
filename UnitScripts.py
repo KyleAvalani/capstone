a0426e7c76263ff936671c2f9402fafb9e528da4
Kyle Avalani <KyleAvalani@users.noreply.github.com>
2020-05-21 16:55:01 -0700

    Update and rename unitOneScript.py to UnitScripts.py

--- UnitScripts.py
+++ UnitScripts.py
@@ -40,7 +40,7 @@ def block_placed_flower_pot():
 blocks.on_block_placed(FLOWER_POT, block_placed_flower_pot)
 def on_chat3():
     mobs.spawn(POLAR_BEAR, world(120, 65, 565))
-player.on_chat("bearslave", on_chat3)
+player.on_chat("bearbuddy", on_chat3)
 def on_forever():
     mobs.apply_effect(NAUSEA,
         mobs.near(mobs.target(NEAREST_PLAYER), world(233, 64, 604), 3),
